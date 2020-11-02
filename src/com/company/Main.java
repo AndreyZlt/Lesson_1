d5947e7ed7d45c6aec098e3215816c71fe287f95
“[Andrey]” <[a.s.zolotukhin@gmail.com]>
2020-11-02 19:41:16 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 3.Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+        // где a, b, c, d – аргументы этого метода, имеющие тип float.
+        Scanner num = new Scanner(System.in);
+        float a, b, c, d, result;
+        System.out.println("Enter a: " );
+        a = num.nextFloat();
+        System.out.println("Enter b: " );
+        b = num.nextFloat();
+        System.out.println("Enter c: " );
+        c = num.nextFloat();
+        System.out.println("Enter d: " );
+        d = num.nextFloat();
+        result = a * (b + (c / d)) ;
+        System.out.println("result is =" + result);
+    }
+}
